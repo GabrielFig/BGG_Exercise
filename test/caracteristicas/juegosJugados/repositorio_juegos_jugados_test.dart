88e795b6b224d63eabc683eec3d390abba59672a
GabrielFig <jgabriell_00@hotmail.com>
2022-10-18 13:23:51 -0600

    add test for benthor plays 5 different games

--- test/caracteristicas/juegosJugados/repositorio_juegos_jugados_test.dart
+++ test/caracteristicas/juegosJugados/repositorio_juegos_jugados_test.dart
@@ -1,7 +1,17 @@
+import 'package:boar_game/caracteristicas/JuegosJugados/repositorio_juegojugado.dart';
+import 'package:boar_game/dominio/variable_dominio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('repositorio juegos jugados ...', (tester) async {
-    // TODO: Implement test
+  test('benthor juega 5 jeugos diferentes', () async {
+    RepositorioJuegosJugadosPruebas repositorio =
+        RepositorioJuegosJugadosPruebas();
+    final resultado = await repositorio
+        .obtenerJugadasRegistradas(NickFormado.constructor('Benthor'));
+    resultado.match((l) {
+      expect(true, equals(false));
+    }, (r) {
+      expect(resultado.length(), equals(5));
+    });
   });
 }
